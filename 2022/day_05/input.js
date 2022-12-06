55f80bd48882c1683ca67586de6cbefe54466029
Andreas Willi <andiwilli8460@gmail.com>
2022-12-06 16:23:45 +0100

    Clean up 2022/day_05 input

--- 2022/day_05/input.js
+++ 2022/day_05/input.js
@@ -1,13 +1,3 @@
-// exports.input = `    [D]
-// [N] [C]
-// [Z] [M] [P]
-//  1   2   3
-
-// move 1 from 2 to 1
-// move 3 from 1 to 3
-// move 2 from 2 to 1
-// move 1 from 1 to 2`;
-// return;
 exports.input = `[T]     [Q]             [S]        
 [R]     [M]             [L] [V] [G]
 [D] [V] [V]             [Q] [N] [C]
