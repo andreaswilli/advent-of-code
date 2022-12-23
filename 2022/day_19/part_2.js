14dd4b9e119ab58224f2f3a71208ed6c8b9444c7
Andreas Willi <andiwilli8460@gmail.com>
2022-12-23 01:03:24 +0100

    Add 2022/day_19

---
+++ 2022/day_19/part_2.js
@@ -0,0 +1,85 @@
+// Day 19: Not Enough Minerals
+// Part Two
+const { input } = require("./input.js");
+const { run } = require("../../util/run.js");
+
+run(() => {
+  let res = 1;
+  const key = (robots, resources, time) => `${robots}-${resources}-${time}`;
+
+  const search = (bp, maxSpend, robots, resources, time) => {
+    const queue = [[robots, resources, time]];
+    const memo = {};
+    let max = 0;
+
+    while (queue.length) {
+      const [robots, resources, time] = queue.pop();
+
+      if (time == 0) max = Math.max(max, resources[3]);
+
+      if (memo[key(robots, resources, time)] != null) continue;
+
+      // only mine
+      max = Math.max(max, resources[3] + robots[3] * time);
+
+      // or craft any robot
+      for (const [robotType, cost] of bp.entries()) {
+        if (robots[robotType] >= maxSpend[robotType]) continue;
+
+        let requiredTime = 0;
+        for (const [resType, amt] of cost) {
+          if (robots[resType] === 0) {
+            requiredTime = Infinity;
+          } else {
+            requiredTime = Math.max(
+              requiredTime,
+              Math.ceil((amt - resources[resType]) / robots[resType])
+            );
+          }
+        }
+        const timeLeft = time - requiredTime - 1;
+        if (timeLeft > 0) {
+          const newRobots = [...robots];
+          newRobots[robotType]++;
+
+          const newResources = [...resources];
+          for (let resType = 0; resType < 4; resType++) {
+            newResources[resType] = Math.min(
+              newResources[resType] + robots[resType] * (requiredTime + 1),
+              maxSpend[resType] * timeLeft
+            );
+          }
+          for (const [resType, amt] of cost) {
+            newResources[resType] -= amt;
+          }
+
+          queue.push([newRobots, newResources, timeLeft]);
+        }
+      }
+      memo[key(robots, resources, time)] = max;
+    }
+    return max;
+  };
+
+  for (const line of input.split("\n").slice(0, 3)) {
+    const maxSpend = [0, 0, 0, Infinity];
+    const bp = line
+      .split(": ")[1]
+      .slice(0, -1)
+      .split(". ")
+      .map((robotType) => {
+        return robotType
+          .split("costs ")[1]
+          .split(" and ")
+          .map((cost) => {
+            let [amt, res] = cost.split(" ");
+            res = ["ore", "clay", "obsidian"].indexOf(res);
+            maxSpend[res] = Math.max(maxSpend[res], amt);
+            return [res, Number(amt)];
+          });
+      });
+    res *= search(bp, maxSpend, [1, 0, 0, 0], [0, 0, 0, 0], 32);
+  }
+
+  return { output: res, expected: 18630 };
+});
