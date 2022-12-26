c1b3254484517ead31918a03dc1a7402b2ed6807
Andreas Willi <andiwilli8460@gmail.com>
2022-12-26 18:05:32 +0100

    Change run util

--- util/run.js
+++ util/run.js
@@ -5,28 +5,23 @@ exports.run = (func) => {
 
   const start = performance.now();
   const result = func();
-  const { output, operations, expected } = result;
+  const { output, expected } = result;
   const time = performance.now() - start;
 
-  if (result.hasOwnProperty("expected")) {
-    console.log("\x1b[31m");
-    console.assert(
-      output === expected,
-      `Output did not match!\n  expected: ${expected}\n  got     : ${output}`
-    );
-    console.log("\x1b[0m");
+  const outputData = [
+    { label: "Output", value: `${output}` },
+    { label: "Time", value: `${formatTime(time, 1)}` },
+  ];
+  let color = "\x1b[0m";
+  if (expected != null) {
+    if (output === expected) {
+      color = "\x1b[32m";
+    } else {
+      outputData.splice(1, 0, { label: "Expected", value: String(expected) });
+      color = "\x1b[31m";
+    }
   }
 
-  const outputData = [
-    { label: "Output value", value: `${output}` },
-    { label: "Finished after", value: `${formatTime(time, 1)}` },
-  ];
-  if (operations != undefined) {
-    outputData.push({
-      label: "Operations",
-      value: `${operations}`.replace(/\B(?=(\d{3})+(?!\d))/g, "'"),
-    });
-  }
   const maxLabelLength = Math.max(
     ...outputData.map((data) => data.label.length)
   );
@@ -41,14 +36,13 @@ exports.run = (func) => {
       )}  ‖`
   );
 
-  console.log(`★ ${"=".repeat(outputLines[0].length - 4)} ★`);
+  console.log(color + `★ ${"=".repeat(outputLines[0].length - 4)} ★`);
   console.log(`‖${" ".repeat(outputLines[0].length - 2)}‖`);
   outputLines.forEach((line) => {
     console.log(line);
   });
   console.log(`‖${" ".repeat(outputLines[0].length - 2)}‖`);
-  console.log(`★ ${"=".repeat(outputLines[0].length - 4)} ★`);
-  console.log("\n");
+  console.log(`★ ${"=".repeat(outputLines[0].length - 4)} ★` + "\x1b[0m");
 
   function formatTime(timeMs, decPlaces) {
     if (timeMs < 1) return `${(timeMs * 1000).toFixed(decPlaces)} μs`;
