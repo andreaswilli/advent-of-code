d2278139ee97c13f8a27aa5bcf647ef26e4faed3
Andreas Willi <andiwilli8460@gmail.com>
2023-12-11 20:03:16 +0100

    Add 2023/day_11

---
+++ 2023/day_11/part_1.js
@@ -0,0 +1,46 @@
+// Day 11: Cosmic Expansion
+// Part One
+const { input } = require("./input.js");
+const { run } = require("../../util/run.js");
+
+run(() => {
+  const grid = input.split("\n");
+  const emptyRows = countEmpty(grid);
+  const rotatedGrid = grid.map((_, i) => grid.map((row) => row[i]).join(""));
+  const emptyCols = countEmpty(rotatedGrid);
+  const galaxies = grid
+    .map((row, y) => row.split("").map((cell, x) => [cell, y, x]))
+    .flat()
+    .filter(([cell]) => cell === "#")
+    .map(([_, y, x]) => [y, x]);
+
+  function countEmpty(grid) {
+    return grid
+      .map((row, i) => [!row.includes("#"), i])
+      .filter(([empty]) => empty)
+      .map(([_, i]) => i);
+  }
+
+  function getExpanded(empty, a, b) {
+    return empty.filter((row) => (a < row && row < b) || (b < row && row < a))
+      .length;
+  }
+
+  function expandedManhattenDistance([a_y, a_x], [b_y, b_x]) {
+    return (
+      Math.abs(a_x - b_x) +
+      Math.abs(a_y - b_y) +
+      getExpanded(emptyRows, a_y, b_y) +
+      getExpanded(emptyCols, a_x, b_x)
+    );
+  }
+
+  let totalDistance = 0;
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      totalDistance += expandedManhattenDistance(galaxies[i], galaxies[j]);
+    }
+  }
+
+  return { output: totalDistance, expected: 9418609 };
+});
