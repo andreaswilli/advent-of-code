232fe356d001f1dee0bd0cc8b2697a0c81ecc029
Andreas Willi <andiwilli8460@gmail.com>
2023-12-13 23:14:46 +0100

    Add 2023/day_12/part_2

---
+++ 2023/day_12/part_2.js
@@ -0,0 +1,56 @@
+// Day 12: Hot Springs
+// Part One
+const { input } = require("./input.js");
+const { run } = require("../../util/run.js");
+
+run(() => {
+  const FACTOR = 5;
+  const rows = input.split("\n").map((row) => {
+    const [springs, nums] = row.split(" ");
+    const newSprings = Array.from({ length: FACTOR }, () => springs).join("?");
+    const newNums = Array.from({ length: FACTOR }, () => nums).join(",");
+    return [newSprings, newNums.split(",").map(Number)];
+  });
+
+  const memo = {};
+
+  function count(springs, nums) {
+    if (springs.length === 0) {
+      return nums.length === 0 ? 1 : 0;
+    }
+
+    if (nums.length === 0) {
+      return springs.includes("#") ? 0 : 1;
+    }
+
+    const key = `${springs}/${nums}`;
+    if (key in memo) return memo[key];
+
+    let result = 0;
+
+    if (".?".includes(springs[0])) {
+      result += count(springs.slice(1), nums);
+    }
+
+    if ("#?".includes(springs[0])) {
+      if (
+        nums[0] <= springs.length &&
+        !springs.slice(0, nums[0]).includes(".") &&
+        (nums[0] === springs.length || springs[nums[0]] !== "#")
+      ) {
+        result += count(springs.slice(nums[0] + 1), nums.slice(1));
+      }
+    }
+
+    memo[key] = result;
+    return result;
+  }
+
+  let sum = 0;
+
+  for (const [springs, nums] of rows) {
+    sum += count(springs, nums);
+  }
+
+  return { output: sum, expected: 7732028747925 };
+});
