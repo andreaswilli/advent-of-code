ce0f2cde03d4c764db1366e25d82cdd5e868b878
Andreas Willi <andiwilli8460@gmail.com>
2023-12-14 01:42:18 +0100

    Improve 2023/day_13

--- 2023/day_13/part_2.js
+++ 2023/day_13/part_2.js
@@ -6,26 +6,25 @@ const { run } = require("../../util/run.js");
 run(() => {
   const patterns = input.split("\n\n").map((pattern) => pattern.split("\n"));
 
-  function findSymmetries(pattern) {
-    const candidates = new Set(
-      Array.from({ length: pattern[0].length - 1 }, (_, i) => i + 1)
-    );
-
-    for (const row of pattern) {
-      for (const c of candidates) {
-        const len = Math.min(c, row.length - c);
-        const left = row.slice(c - len, c);
+  function findSymmetry(pattern) {
+    for (let i = 1; i < pattern[0].length; i++) {
+      const totalDiff = pattern.reduce((diff, row) => {
+        const len = Math.min(i, row.length - i);
+        const left = row.slice(i - len, i);
         const right = row
-          .slice(c, c + len)
+          .slice(i, i + len)
           .split("")
           .reverse()
           .join("");
-        if (left !== right) {
-          candidates.delete(c);
-        }
+        const localDiff = left
+          .split("")
+          .reduce((acc, l, i) => (acc + (l === right[i] ? 0 : 1)), 0);
+        return diff + localDiff;
+      }, 0);
+      if (totalDiff === 1) {
+        return i;
       }
     }
-    return Array.from(candidates);
   }
 
   function rotatePattern(pattern) {
@@ -34,53 +33,15 @@ run(() => {
       .map((_, i) => pattern.map((row) => row[row.length - 1 - i]).join(""));
   }
 
-  function fixSmudge(pattern, smudgeRow, smudgeCol) {
-    return pattern.map((row, i) =>
-      i !== smudgeRow
-        ? row
-        : row
-            .split("")
-            .map((spot, j) =>
-              j !== smudgeCol ? spot : spot === "#" ? "." : "#"
-            )
-            .join("")
-    );
-  }
-
   let sum = 0;
 
   for (const pattern of patterns) {
-    let vertical = [];
-    let horizontal = [];
-    const excludeVertical = findSymmetries(pattern)[0];
-    const excludeHorizontal = findSymmetries(rotatePattern(pattern))[0];
-
-    for (
-      let row = 0;
-      row < pattern.length && !vertical.length && !horizontal.length;
-      row++
-    ) {
-      for (
-        let col = 0;
-        col < pattern[row].length && !vertical.length && !horizontal.length;
-        col++
-      ) {
-        const fixedPattern = fixSmudge(pattern, row, col);
-        vertical = findSymmetries(fixedPattern).filter(
-          (v) => v !== excludeVertical
-        );
-
-        if (vertical.length) {
-          sum += vertical[0];
-        } else {
-          horizontal = findSymmetries(rotatePattern(fixedPattern)).filter(
-            (h) => h !== excludeHorizontal
-          );
-          if (horizontal.length) {
-            sum += horizontal[0] * 100;
-          }
-        }
-      }
+    const vertical = findSymmetry(pattern);
+    if (vertical != null) {
+      sum += vertical;
+    } else {
+      const horizontal = findSymmetry(rotatePattern(pattern));
+      sum += horizontal * 100;
     }
   }
 
