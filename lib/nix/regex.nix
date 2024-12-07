3aa1ffb5856d092567b51bdc25d640de76207792
Andreas Willi <andiwilli8460@gmail.com>
2024-12-07 15:32:53 +0100

    Simplify some lib functions

--- lib/nix/regex.nix
+++ lib/nix/regex.nix
@@ -1,26 +1,21 @@
 with (import <nixpkgs> { }).lib;
-let
-  _matchAll =
-    matches: pattern: str:
+rec {
+  matchAll =
+    pattern: str:
     let
       m = match ".*(${pattern})(.*)" str;
       idxMatch = stringLength str - stringLength (head m) - stringLength (last m);
     in
     if (m == null) then
-      matches
+      [ ]
     else
-      _matchAll (
-        [
-          {
-            idx = idxMatch;
-            val = (sublist 1 (length m - 2) m);
-          }
-        ]
-        ++ matches
-      ) pattern (substring 0 idxMatch str);
-in
-rec {
-  matchAll = _matchAll [ ];
+      matchAll pattern (substring 0 idxMatch str)
+      ++ [
+        {
+          idx = idxMatch;
+          val = (sublist 1 (length m - 2) m);
+        }
+      ];
   matchAllValue = pattern: str: matchAll pattern str |> map (getAttr "val");
   matchAllIndex = pattern: str: matchAll pattern str |> map (getAttr "idx");
 }
